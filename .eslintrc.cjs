9335be959cd5af3c3ee92e2cdc2173b8f612e40d
Kamil Bażanow <kamil.bazanow@gmail.com>
2024-06-24 10:18:33 +0200

    chore: initial project setup

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,16 +6,50 @@ module.exports = {
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
+    'plugin:import/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:jest/recommended',
+    'plugin:jest-dom/recommended',
+    'plugin:react/jsx-runtime',
+    'prettier',
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  ignorePatterns: ['dist', '.eslintrc.cjs', 'coverage', 'node_module'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
+  plugins: [
+    'react-refresh',
+    'import',
+    'simple-import-sort',
+    'jsx-a11y',
+    'jest',
+    'jest-dom',
+    'testing-library',
+  ],
   rules: {
+    'import/no-empty-named-blocks': 'error',
+    'import/first': 'error',
+    'import/newline-after-import': 'error',
+    'import/no-duplicates': 'error',
+    'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error',
     'react/jsx-no-target-blank': 'off',
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
   },
-}
+  settings: {
+    'import/resolver': {
+      alias: {
+        map: [['', './public']],
+        extensions: ['.js', '.jsx'],
+      },
+    },
+  },
+  overrides: [
+    {
+      files: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
+      extends: ['plugin:testing-library/react'],
+    },
+  ],
+};
---
+++ src/App.test.jsx
@@ -0,0 +1,13 @@
+import { render, screen } from '@testing-library/react';
+
+import App from './App';
+
+describe('App', () => {
+  it('should test', () => {
+    render(<App></App>);
+
+    const heading = screen.getByRole('heading');
+
+    expect(heading).toBeInTheDocument();
+  });
+});
