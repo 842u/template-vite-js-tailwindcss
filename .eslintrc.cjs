cf2670959480453845fe25db1cd425f303932fc6
Kamil Bażanow <kamil.bazanow@gmail.com>
2024-06-24 15:32:08 +0200

    chore(eslint): remove redundant extend

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,7 +4,6 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
     'plugin:import/recommended',
     'plugin:jsx-a11y/recommended',
